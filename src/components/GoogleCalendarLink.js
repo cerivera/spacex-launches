7577dc0e1f35a8631b5ea6904bd4cd0471b2d27c
Carlos Rivera <crivera1@underarmour.com>
2018-11-06 21:48:10 -0600

    Refactored GoogleCal Link

---
+++ src/components/GoogleCalendarLink.js
@@ -0,0 +1,42 @@
+import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+// To ensure single digits have a leading zero
+const padNum = (num) => num.toString().padStart(2, '0');
+
+const buildDatesParam = (startDate, durationInHours) => {
+  // Oh ya I did.
+  // Formatting these dates the "right" way was awful.
+  const formattedStartDate = startDate.replace(/\.\d\d\d/, '').replace(/[-:.]*/g, '')
+  const formattedEndDate = formattedStartDate.replace(/T(\d\d)/, (_, m1) => {
+    return `T${padNum(parseInt(m1)+durationInHours)}`
+  })
+  return `${formattedStartDate}/${formattedEndDate}`
+};
+
+const GoogleCalendarLink = (props) => {
+  const encodedDates = buildDatesParam(props.eventStartDate, props.durationInHours);
+  const encodedDescription = encodeURIComponent(props.description);
+  const encodedLocation = encodeURIComponent(props.location);
+  const encodedTitle = encodeURIComponent(props.title);
+
+  return (
+    <Button
+      size="small"
+      variant="contained"
+      color="primary"
+      href={`http://www.google.com/calendar/event?action=TEMPLATE&text=${encodedTitle}&dates=${encodedDates}&details=${encodedDescription}&location=${encodedLocation}`}
+    >REMIND ME</Button>
+  )
+};
+
+GoogleCalendarLink.propTypes = {
+  eventStartDate: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  durationInHours: PropTypes.number.isRequired,
+  location: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default GoogleCalendarLink;
--- src/components/LaunchesRow.js
+++ src/components/LaunchesRow.js
@@ -1,8 +1,9 @@
+import Button from '@material-ui/core/Button';
+import GoogleCalendarLink from './GoogleCalendarLink';
 import PropTypes from 'prop-types';
 import React from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import {CalendarLink, GenericLink} from './Links';
 
 class LaunchesRow extends React.Component {
   render() {
@@ -17,18 +18,29 @@ class LaunchesRow extends React.Component {
         <TableCell>{item.details}</TableCell>
         <TableCell>
           {item.upcoming
-            ? <CalendarLink
-                title={`SpaceX Launch - ${item.mission_name}`}
+            ? <GoogleCalendarLink
+                eventStartDate={item.launch_date_utc}
                 description="https://apps.cerivera.com/spacex"
-                date={item.launch_date_utc}
+                durationInHours={2}
                 location={item.launch_site.site_name_long}
+                title={`SpaceX Launch - ${item.mission_name}`}
               />
             : null}
           {links.reddit_campaign
-            ? <GenericLink link={links.reddit_campaign} title="DISCUSS" />
+            ? <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                href={links.reddit_campaign}
+              >DISCUSS</Button>
             : null}
           {links.video_link 
-            ? <GenericLink link={links.video_link} title="WATCH" />
+           ? <Button
+                size="small"
+                variant="contained"
+                color="primary"
+                href={links.video_link}
+             >WATCH</Button>
             : null}
         </TableCell>
       </TableRow>
